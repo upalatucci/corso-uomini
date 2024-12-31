d9e4147cca6d3536c4cecce4f35e95b555738974
Ugo Palatucci <upalatuc@redhat.com>
2024-12-31 11:06:54 +0100

    roboto

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,6 @@ import ScrollUp from "@/components/Common/ScrollUp";
 import Daimoku from "@/components/Daimoku";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Corso Uomini 2025",
-  description:
-    "Vinciamo in questo “Anno in cui volare ancora più in alto per una Soka Gakkai giovane in tutto il mondo”",
-  // other metadata
-};
 
 export default function Home() {
   return (
