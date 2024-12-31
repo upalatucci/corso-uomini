96370d96ae37002fc96c419dae9bfbff50cda4d8
Ugo Palatucci <upalatuc@redhat.com>
2024-12-31 11:00:29 +0100

    new features

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -1,7 +1,7 @@
 import { Feature } from "@/types/feature";
 
 const SingleFeature = ({ feature }: { feature: Feature }) => {
-  const { icon, title, paragraph } = feature;
+  const { icon, title, paragraph, paragraphHTML } = feature;
   return (
     <div className="w-full">
       <div className="wow fadeInUp" data-wow-delay=".15s">
@@ -11,9 +11,16 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         <h3 className="mb-5 text-xl font-bold text-black sm:text-2xl lg:text-xl xl:text-2xl">
           {title}
         </h3>
-        <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color">
-          {paragraph}
-        </p>
+        {paragraphHTML ? (
+          <p
+            dangerouslySetInnerHTML={{ __html: paragraphHTML }}
+            className="pr-[10px] text-base font-medium leading-relaxed text-body-color"
+          />
+        ) : (
+          <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color">
+            {paragraph}
+          </p>
+        )}
       </div>
     </div>
   );
--- types/feature.ts
+++ types/feature.ts
@@ -4,5 +4,6 @@ export type Feature = {
   id: number;
   icon: JSX.Element;
   title: string;
-  paragraph: string;
+  paragraph?: string;
+  paragraphHTML?: string;
 };
