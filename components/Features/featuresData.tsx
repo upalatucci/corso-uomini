f1a7954308e121b1515f1b94157f861d1eb2b451
Ugo Palatucci <upalatuc@redhat.com>
2025-01-11 09:37:34 +0100

    parcheggi

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -7,6 +7,7 @@ import {
   CurrencyEuroIcon,
   EnvelopeIcon,
   MapIcon,
+  MapPinIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
 
@@ -25,6 +26,16 @@ const featuresData: Feature[] = [
     paragraphHTML: `<a class='text-blue-600 hover:underline' target='_blank' href='https://www.google.com/maps/place/Grand+Hotel+Salerno/@40.6725175,14.7733016,17z/data=!3m1!4b1!4m9!3m8!1s0x133bc246d5b3550b:0x14eeac8b514c5197!5m2!4m1!1i2!8m2!3d40.6725135!4d14.7758765!16s%2Fg%2F1tj5515h?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D'>
        Grand Hotel Salerno via Lungomare Clemente Tafuri, 1 - 84127 Salerno (SA)
       </a> con parcheggio <a class='text-blue-600 hover:underline' target='_blank' href='https://www.google.com/maps/place/Via+Torrione,+6-80,+84127+Salerno+SA/@40.6731502,14.7749734,18z/data=!3m1!4b1!4m17!1m10!3m9!1s0x133bc246d5b3550b:0x14eeac8b514c5197!2sGrand+Hotel+Salerno!5m2!4m1!1i2!8m2!3d40.6725135!4d14.7758765!16s%2Fg%2F1tj5515h!3m5!1s0x133bc2476c3a2413:0x194f102b39a83728!8m2!3d40.6731482!4d14.7762257!16s%2Fg%2F11w36_cs09?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D'>qui</a>`,
+  },
+  {
+    id: 10,
+    icon: <MapPinIcon />,
+    title: "Parcheggio",
+    paragraphHTML: `<strong>Parcheggio via A. Carella</strong>: aperto ( con parcometro), adiacente Hotel Salerno, Tariffa 1€ ogni 2 ore, (aperto H24)<br />
+<strong>Parcheggio Vinciprova: chiuso  </strong>( con sbarre), a circa 100 m dall' Hotel Salerno, Tariffa 1 € ogni 2 ore, (aperto H 24 )<br />
+<strong>Parcheggio Foce Irno </strong>: 
+Chiuso (con sbarre), con ingresso da via Torrione, adiacente Hotel Salerno, Tariffe 1€ ogni ora ( aperto H 24 )<br />
+<strong>Parcheggio interrato Foce Irno Chiuso</strong> ( con sbarre), con ingresso da via lungomare, adiacente Hotel Salerno, tariffa diurna 1 € ogni ora, tariffa notturna 1 € ogni 2 ore, (aperto H 24 ).`,
   },
   {
     id: 2,
