dc9f33eaf363850d64aa2dfaba5a3f0a464488b2
Ugo Palatucci <upalatuc@redhat.com>
2025-02-01 16:35:57 +0100

    roboto

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,12 +1,4 @@
-import { Permanent_Marker, Roboto } from "next/font/google";
-
-export const permanentMarker = Permanent_Marker({
-  weight: ["400"],
-  style: ["normal"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-permanent-marker",
-});
+import { Roboto } from "next/font/google";
 
 export const roboto = Roboto({
   weight: ["400", "700"],
