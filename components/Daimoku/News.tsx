8c6a93e9152a4457d290ab7abe52124c9c5b79c8
Ugo Palatucci <upalatuc@redhat.com>
2025-02-05 14:39:13 +0100

    remove stay tuned

--- components/Daimoku/News.tsx
+++ components/Daimoku/News.tsx
@@ -5,8 +5,7 @@ const News: FC = () => {
     <div className="w-full md:w-2/3 mb-10 md:border-r-gray-400 md:border-r-8 p-4">
       <div className="flex justify-center align-middle">
         <span className="text-4xl text-primary font-bold">
-          Prossimamente pubblicheremo in questo spazio le varie news! Stay
-          tuned!
+          Prossimamente pubblicheremo in questo spazio le varie news!
         </span>
       </div>
     </div>
